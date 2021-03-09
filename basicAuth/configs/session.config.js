1187aa689f27f204517a2d6ba446a2d689841562
Ironhack Education Team <admin.education@ironhack.com>
2021-03-09 16:55:14 +0100

    Update session.config.js

--- basicAuth/configs/session.config.js
+++ basicAuth/configs/session.config.js
@@ -4,7 +4,7 @@
 const session = require('express-session');
 
 // ADDED: require mongostore
-const MongoStore = require('connect-mongo')(session);
+const MongoStore = require('connect-mongo').default;
 
 // ADDED: require mongoose
 const mongoose = require('mongoose');
@@ -28,10 +28,12 @@ module.exports = app => {
         httpOnly: true,
         maxAge: 60000 // 60 * 1000 ms === 1 min
       },
-      store: new MongoStore({
-        mongooseConnection: mongoose.connection,
+      store: MongoStore.create({
+        // <== ADDED !!!
+        mongoUrl: process.env.MONGODB_URI
+
         // ttl => time to live
-        ttl: 60 * 60 * 24 // 60sec * 60min * 24h => 1 day
+        // ttl: 60 * 60 * 24 // 60sec * 60min * 24h => 1 day
       })
     })
   );
