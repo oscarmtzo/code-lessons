e3f6717a18c356eb42b65cad145772c0b500b053
sandrabosk <sandraboskovic@hotmail.com>
2021-04-28 12:41:27 +0200

    minor change to make it more readable

--- basicAuth/configs/session.config.js
+++ basicAuth/configs/session.config.js
@@ -7,12 +7,14 @@ const MongoStore = require('connect-mongo');
 const mongoose = require('mongoose');
 // since we are going to USE this middleware in the app.js,
 // let's export it and have it receive a parameter
-module.exports = app => {
+module.exports = (app) => {
   //              |
   //              app is just a placeholder here but will become a real "app"
   //              in the app.js when this file gets imported/required there
+
   // required for the app when deployed to Heroku (in production)
   app.set('trust proxy', 1);
+
   // use session
   app.use(
     session({
@@ -24,14 +26,14 @@ module.exports = app => {
         sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
         secure: process.env.NODE_ENV === 'production',
         httpOnly: true,
-        maxAge: 60000 // 60 * 1000 ms === 1 min
+        maxAge: 60000, // 60 * 1000 ms === 1 min
       },
       store: MongoStore.create({
         // <== ADDED !!!
-        mongoUrl: process.env.MONGODB_URI || 'mongodb://localhost/basicAuth'
+        mongoUrl: process.env.MONGODB_URI || 'mongodb://localhost/basicAuth',
         // ttl => time to live
         // ttl: 60 * 60 * 24 // 60sec * 60min * 24h => 1 day
-      })
+      }),
     })
   );
 };
